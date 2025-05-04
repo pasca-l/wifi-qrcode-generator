a6c8a0f068b7e0e3d3e101f68e84484b46c20c78
pasca-l <shion.yamadate@keio.jp>
2025-05-04 14:52:54 +0900

    updated image drawing

--- src/qrcode/svg.go
+++ src/qrcode/svg.go
@@ -7,12 +7,20 @@ import (
 	svg "github.com/ajstarks/svgo"
 )
 
+const imageSize int = 300
 const black string = "#000000"
 
 func DrawQRCode(w io.Writer, code QRCode) error {
 	s := svg.New(w)
-	s.Start(300, 300)
-	s.Square(0, 0, 300, fmt.Sprintf(`fill="%s"`, black))
+	s.Start(imageSize, imageSize)
+	for y, row := range code.Pattern {
+		for x, cell := range row {
+			pixel := imageSize / len(code.Pattern)
+			if cell {
+				s.Square(x*pixel, y*pixel, pixel, fmt.Sprintf(`fill="%s"`, black))
+			}
+		}
+	}
 	s.End()
 
 	return nil
